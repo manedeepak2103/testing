018c263fc8921270a60e2582a1419b00a101b8d2
Deepak Mane <manedeepak2103@gmail.com>
2023-06-26 21:39:02 +0530

    Initial commit

--- api.js
+++ api.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const app = express();
+
 const router = express.Router();
+
 // Handle incoming POST requests to the '/webhook' endpoint
-app.post('/webhook', (req, res) => {
+router.post('/webhook', (req, res) => {
   // Process the webhook payload
   // Add your webhook handling logic here
-    console.log(req.body)
+  console.log(req.body);
+
   // Send a response back to the webhook sender
-  res.sendStatus(200);
+  res.json({ message: 'Webhook received successfully' });
 });
-router.get("/", (req, res) => {
-    res.json({
-      hello: "hi!"
-    });
-  });
-  
-  app.use(`/.netlify/functions/api`, router);
+
+// Mount the router on the '/api' base URL
+app.use('/api', router);
+
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
